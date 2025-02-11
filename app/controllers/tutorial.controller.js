bbbd07169b093c53b0423666a2ed6f856df25e49
armenski <mihail.armenski@outlook.com>
2025-02-11 18:27:56 +0100

    errorMSG

--- app/controllers/tutorial.controller.js
+++ app/controllers/tutorial.controller.js
@@ -7,7 +7,7 @@ exports.create = (req, res) => {
   if (!req.body.title) {
     res.status(400).send({ message: "Content can not be empty!" });
     return;
-  }
+  
 
   // Create a Tutorial
   const tutorial = new Tutorial({
